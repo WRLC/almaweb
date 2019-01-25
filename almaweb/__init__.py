6f65035b3040832a5e64c3e5813cfa97c4c41c55
ihardy <ihardy@wrlc.org>
2019-01-25 14:15:46 -0500

    adds bootstrap

--- almaweb/__init__.py
+++ almaweb/__init__.py
@@ -1,10 +1,12 @@
 import os
 
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+    Bootstrap(app)
     app.config.from_mapping(
         SECRET_KEY='dev',
     )
@@ -27,5 +29,6 @@ def create_app(test_config=None):
     from . import index
     app.register_blueprint(item.bp)
     app.register_blueprint(index.bp)
+
 
     return app
